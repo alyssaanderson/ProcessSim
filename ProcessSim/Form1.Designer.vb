2d4f056cda08dd473fb420d02248fbd9bb352b73
Alyssa Anderson <wackyvorlon@gmail.com>
2020-07-31 11:29:57 -0400

    Changed window title.

--- ProcessSim/Form1.Designer.vb
+++ ProcessSim/Form1.Designer.vb
@@ -94,7 +94,7 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Process Simulator"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
