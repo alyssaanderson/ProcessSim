7aa0fbfbfaca17a97862a96300413663fc225665
Alyssa Anderson <wackyvorlon@gmail.com>
2020-07-31 14:09:35 -0400

    Process variable now reflects the level of the tank. Created a button to manually increase level in tank.

--- ProcessSim/Form1.Designer.vb
+++ ProcessSim/Form1.Designer.vb
@@ -29,6 +29,7 @@ Partial Class Form1
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.TrackBar1 = New System.Windows.Forms.TrackBar()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -94,11 +95,21 @@ Partial Class Form1
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "Tank Level"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(311, 247)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 32)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Increase"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.TrackBar1)
         Me.Controls.Add(Me.CheckBox1)
@@ -120,4 +131,5 @@ Partial Class Form1
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents TrackBar1 As TrackBar
     Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- ProcessSim/Form1.vb
+++ ProcessSim/Form1.vb
@@ -4,4 +4,18 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TrackBar1.Value <= 99 Then
+            TrackBar1.Value = TrackBar1.Value + 1
+        Else
+            TrackBar1.Value = 0
+        End If
+
+
+    End Sub
+
+    Private Sub TrackBar1_ValueChanged(sender As Object, e As EventArgs) Handles TrackBar1.ValueChanged
+        Label2.Text = TrackBar1.Value
+    End Sub
 End Class
